98d59bf8f84b29ed22f35dd48174ed9d1ca4cc14
AnDong <dongphuocan123@gmail.com>
2023-05-09 14:06:59 +0700

    add notification

---
+++ src/main/java/com/example/notificationservice/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.example.notificationservice.service;
+
+import com.example.notificationservice.model.MessageDTO;
+
+public interface EmailService {
+
+	void sendEmail(MessageDTO messageDTO);
+}
---
+++ src/main/java/com/example/notificationservice/service/MessageService.java
@@ -0,0 +1,24 @@
+package com.example.notificationservice.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.example.notificationservice.model.MessageDTO;
+
+@Service
+public class MessageService {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private EmailService emailService;
+	
+	@KafkaListener(id = "notificationGroup", topics = "notification")
+	public void listen(MessageDTO messageDTO) {
+		logger.info("Received: ", messageDTO.getTo());
+		emailService.sendEmail(messageDTO);
+	}
+}
